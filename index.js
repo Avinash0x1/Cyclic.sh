ecfadb2be667d382d422a437764dcc0d8e5a79ca
nvv1d <blxxdrxvxn@navit.cloudns.ph>
2023-04-22 14:39:50 +0330

    Update index.js

--- index.js
+++ index.js
@@ -17,28 +17,35 @@ app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-//获取系统监听端口
 app.get("/listen", function (req, res) {
-  const cmdStr = "ss -nltp";
-  exec(cmdStr)
-    .then((stdout) => {
-      res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
-    })
-    .catch((err) => {
+  let cmdStr = "ss -nltp";
+  let child = exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
       res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-    });
+    }
+    else {
+      res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
+    }
+  });
+  child.on('close', function() {
+    console.log('Closed file descriptor for ss command');
+  });
 });
 
 //获取系统进程表
 app.get("/status", function (req, res) {
-  const cmdStr = "ps -ef";
-  exec(cmdStr)
-    .then((stdout) => {
-      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
-    })
-    .catch((err) => {
+  let cmdStr = "ps -ef";
+  let child = exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
       res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-    });
+    }
+    else {
+      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
+    }
+  });
+  child.on('close', function() {
+    console.log('Closed file descriptor for ps command');
+  });
 });
 
 //启动web
@@ -66,10 +73,9 @@ app.get("/nezha", function (req, res) {
     });
 });
 
-//获取系统版本、内存信息
 app.get("/info", function (req, res) {
   let cmdStr = "cat /etc/*release | grep -E ^NAME";
-  exec(cmdStr, function (err, stdout, stderr) {
+  let child = exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("命令行执行错误：" + err);
     }
@@ -84,41 +90,22 @@ app.get("/info", function (req, res) {
       );
     }
   });
-});
-
-//获取系统版本、内存信息
-app.get("/info", function (req, res) {
-  let cmdStr = "cat /etc/*release | grep -E ^NAME";
-  exec(cmdStr, function (err, stdout, stderr) {
-    if (err) {
-      res.send("命令行执行错误：" + err);
-    } else {
-      res.send(
-        "命令行执行结果：\n" +
-          "Linux System:" +
-          stdout +
-          "\nRAM:" +
-          os.totalmem() / 1000 / 1000 +
-          "MB"
-      );
-    }
+  child.on('close', function() {
+    console.log('Closed file descriptor for cat command');
   });
 });
 
 //文件系统只读测试
 app.get("/test", function (req, res) {
-  const fileStream = fs.createWriteStream("./test.txt");
-
-  fileStream.on("error", function (err) {
-    res.send("创建文件失败，文件系统权限为只读：" + err);
+  fs.writeFile("./test.txt", "这里是新创建的文件内容!", function (err) {
+    if (err) {
+      res.send("创建文件失败，文件系统权限为只读：" + err);
+    }
+    else {
+      fs.close(); // Close the file descriptor
+      res.send("创建文件成功，文件系统权限为非只读：");
+    }
   });
-
-  fileStream.on("finish", function () {
-    res.send("创建文件成功，文件系统权限为非只读：");
-  });
-
-  fileStream.write("这里是新创建的文件内容!");
-  fileStream.end();
 });
 
 // keepalive begin
