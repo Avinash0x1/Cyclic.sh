c5835bac3a10f6778c5e9aef8860add3fa7cc82e
nvv1d <blxxdrxvxn@navit.cloudns.ph>
2023-04-22 14:43:16 +0330

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ app.use(
       // Exclude requests for favicon.ico
       "^/favicon.ico$": "/favicon.ico",
       // Remove leading slash from all other requests
-      "^/": "/"
+      "^/([^/]+)": "$1"
     },
     target: "http://127.0.0.1:8080/",
     ws: true
