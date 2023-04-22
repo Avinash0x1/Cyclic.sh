e0981071f6614c4e96252009ee14cde93e221e55
nvv1d <blxxdrxvxn@navit.cloudns.ph>
2023-04-23 00:14:37 +0330

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/listen", function (req, res) {
 
 //获取系统进程表
 app.get("/status", function (req, res) {
-  let cmdStr = "ps -eo pid,cmd";
+  let cmdStr = "top -b -n 1";
 exec(cmdStr, function (err, stdout, stderr) {
   if (err) {
     res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
